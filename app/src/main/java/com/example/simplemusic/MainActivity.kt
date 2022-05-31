37dc3ecbe941752baea9c4098aafab74ca633b59
pinocchio22 <vviian.2@gmail.com>
2022-05-31 21:49:19 +0900

    URI 이용

--- app/src/main/java/com/example/simplemusic/MainActivity.kt
+++ app/src/main/java/com/example/simplemusic/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.simplemusic
 
+import android.media.AudioManager
 import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -11,5 +13,13 @@ class MainActivity : AppCompatActivity() {
 
         val musicPlayer : MediaPlayer? = MediaPlayer.create(this, R.raw.chocolate)
         musicPlayer?.start()
+
+        val myUri : Uri = Uri.parse("uri")//"uri 초기화"
+        val mediaPlayer : MediaPlayer? = MediaPlayer().apply {
+            setAudioStreamType(AudioManager.STREAM_MUSIC)
+            setDataSource(applicationContext, myUri)
+            prepare()
+            start()
+        }
     }
 }
